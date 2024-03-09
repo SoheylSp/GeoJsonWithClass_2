7d4f5a326ebc89ac59f1dab15adea5960b6a9586
SoheylSp <144216711+SoheylSp@users.noreply.github.com>
2024-03-09 17:36:56 +0330

    Add files via upload

--- GeoJsonWithClass/geojsonclass.cpp
+++ GeoJsonWithClass/geojsonclass.cpp
@@ -1,5 +1,7 @@
 #include "geojsonclass.h"
 
+
+
 GeojsonClass::GeojsonClass(const QString& filename):filename_(filename)
 {
 
@@ -71,6 +73,99 @@ void GeojsonClass::addX2(int lat1, int lon1)
     features_.append(feature) ;
 }
 
+void GeojsonClass::addPolygon(QGeoCoordinate point1, QGeoCoordinate point2, QGeoCoordinate point3, QGeoCoordinate point4 , QGeoCoordinate point5)
+{
+
+    QJsonObject feature ;
+    QJsonObject properties ;
+    QJsonObject geometry ;
+    QJsonArray coordinates ;
+
+    feature["type"]="Feature";
+    feature["properties"] = properties ;
+    geometry["type"] = "Polygon" ;
+
+    coordinates.append(QJsonArray({point1.latitude(),point1.longitude()}));
+    coordinates.append(QJsonArray({point2.latitude(),point2.longitude()}));
+    coordinates.append(QJsonArray({point3.latitude(),point3.longitude()}));
+    coordinates.append(QJsonArray({point4.latitude(),point4.longitude()}));
+    coordinates.append(QJsonArray({point5.latitude(),point5.longitude()}));
+    coordinates.append(QJsonArray({point1.latitude(),point1.longitude()})); // closing the polygon by adding the first point again
+
+    geometry["coordinates"]=QJsonArray({coordinates});
+    feature["geometry"] = geometry ;
+
+    features_.append(feature) ;
+
+
+}
+
+void GeojsonClass::addCircle(const QGeoCoordinate &center, double radius)
+{
+
+    QJsonObject feature ;
+    QJsonObject properties ;
+    QJsonObject geometry ;
+    QJsonArray coordinates ;
+
+    feature["type"] = "Feature";
+    feature["properties"] = properties ;
+    geometry["type"] = "Polygon" ;
+
+    for(int i = 0 ; i <360 ; i+= 10){
+
+        QGeoCoordinate point = center.atDistanceAndAzimuth(radius,i) ;
+        coordinates.append(QJsonArray{point.longitude(),point.latitude()}) ;
+
+
+    }
+
+    //close the circle by adding the first point again
+
+    coordinates.append(coordinates.at(0)) ;
+
+    geometry["coordinates"] = QJsonArray({coordinates});
+    feature["geometry"] = geometry ;
+
+    features_.append(feature) ;
+
+
+    
+}
+
+void GeojsonClass::calculateGreatCircle(const QGeoCoordinate &point1, const QGeoCoordinate &point2, int numPoints)
+{
+
+    {
+        for (int i = 0; i <= numPoints; ++i) {
+            double f = static_cast<double>(i) / numPoints; // calculate the fraction between points from point1 to point2
+            double distance = point1.distanceTo(point2);
+            double azimuth = point1.azimuthTo(point2);
+
+            QGeoCoordinate intermediatePoint = point1.atDistanceAndAzimuth(distance * f, azimuth);
+
+            QJsonObject feature;
+            QJsonObject properties;
+            QJsonObject geometry;
+            QJsonArray coordinates;
+
+            feature["type"] = "Feature";
+            feature["properties"] = properties;
+            geometry["type"] = "Point";
+
+            coordinates.append(intermediatePoint.longitude());
+            coordinates.append(intermediatePoint.latitude());
+
+            geometry["coordinates"] = coordinates;
+            feature["geometry"] = geometry;
+
+            features_.append(feature);
+        }
+    }
+
+}
+
+
 
 void GeojsonClass::writeToFile()
 {
@@ -79,7 +174,6 @@ void GeojsonClass::writeToFile()
 
         qWarning()<<"Faild to open file for writing";
         return ;
-
 
     }
     geojson_["features"]= features_;
